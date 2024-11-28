3da249dd186acf7ab0fdccadecd2e8f3600f927e
stasimus78 <stas.od@gmail.com>
2024-11-28 14:25:34 +0200

    7. Добавление компонента LogoCloud на главную страницу для отображения логотипов учебных заведений

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/frontend/hero-section";
+import LogoCloud from "@/components/frontend/logo-cloud";
 
 export default function HomePage() {
   return (
     <main className="">
       <HeroSection />
+      <LogoCloud />
     </main>
   );
 }
