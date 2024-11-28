8c528998d6f3de187370e5a651ae75e83b5eb3db
stasimus78 <stas.od@gmail.com>
2024-11-28 12:28:50 +0200

    1. Замена шрифтов на Rethink Sans и удаление неиспользуемых SVG файлов

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,11 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+import type { Metadata } from "next";
+import { Rethink_Sans } from "next/font/google";
+
+const rethinkSans = Rethink_Sans({
+  subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -25,11 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={rethinkSans.className}>{children}</body>
     </html>
   );
 }
