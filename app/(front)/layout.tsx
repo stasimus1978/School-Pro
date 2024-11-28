951f98fc943f276fe7755228c63f8539f6d6fb07
stasimus78 <stas.od@gmail.com>
2024-11-28 13:01:34 +0200

    3. Добавление утилиты для объединения классов, обновление макета фронтенда с заголовком, настройка конфигурации Tailwind и добавление кнопки с вариантами стилей

--- app/(front)/layout.tsx
+++ app/(front)/layout.tsx
@@ -1,5 +1,11 @@
+import SiteHeader from "@/components/site-header";
 import { ReactNode } from "react";
 
 export default function FrontLayout({ children }: { children: ReactNode }) {
-  return <div className="">{children}</div>;
+  return (
+    <div className="">
+      <SiteHeader />
+      {children}
+    </div>
+  );
 }
