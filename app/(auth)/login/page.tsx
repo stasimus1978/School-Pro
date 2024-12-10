551f571666c8908fc339d31f304248e87952ada9
stasimus1978 <stas.od@gmail.com>
2024-12-10 23:41:28 +0200

    78. Добавление редиректа для авторизованных пользователей на страницу панели управления при входе

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,6 +1,14 @@
+import { getServerUser } from "@/actions/users";
 import Login from "@/components/frontend/auth/login";
+import { redirect } from "next/navigation";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const user = await getServerUser();
+
+  if (user) {
+    redirect("/dashboard");
+  }
+
   return (
     <div className="">
       <Login />
